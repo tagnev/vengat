b803eda88a4cb593c4f915e756d7edca64a6711f
LetsLearn <tagnev.vengat@gmail.com>
2023-11-07 12:47:37 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,88 +1,99 @@
 import streamlit as st
 
-# Configuring the page
-st.set_page_config(page_title="Vengateswaran Arunachalam's Resume", page_icon=":briefcase:", layout="wide")
+# Set page config
+st.set_page_config(page_title="Vengateswaran Arunachalam's Resume", layout="wide")
 
-# Defining custom styles for the resume
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+# Main title
+st.image('https://media.licdn.com/dms/image/D5603AQGzch8kk3Cs1A/profile-displayphoto-shrink_200_200/0/1696458140217?e=1704931200&v=beta&t=IC1H7RGnR3fJFLhMeWRZq95-_VWJH7yh2HxGGUNh0dg', width=120)
+st.title('Vengateswaran Arunachalam')
+st.write('Santa Clara, CA 95051 | (408)784-9915 | tagnev.vengat@gmail.com')
+st.markdown('---')
 
-# Assuming you have a styles.css file in the same directory with your desired CSS
-# local_css("styles.css")
-
-# Using raw HTML for custom styling, if you don't have a styles.css file
-st.markdown("""
-<style>
-body {
-    font-family: 'Helvetica', sans-serif;
-}
-.header {
-    font-weight: bold;
-    font-size: 36px;
-    line-height: 1.6;
-}
-.subheader {
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 1.4;
-}
-.section {
-    margin-bottom: 20px;
-}
-.detail-text {
-    font-size: 16px;
-    line-height: 1.6;
-}
-.sidebar-text {
-    font-size: 14px;
-    line-height: 1.5;
-}
-</style>
-""", unsafe_allow_html=True)
-
-# Resume Header
-st.image('https://media.licdn.com/dms/image/D5603AQGzch8kk3Cs1A/profile-displayphoto-shrink_200_200/0/1696458140217?e=1704931200&v=beta&t=IC1H7RGnR3fJFLhMeWRZq95-_VWJH7yh2HxGGUNh0dg', width=150)
-st.markdown('<p class="header">Vengateswaran Arunachalam</p>', unsafe_allow_html=True)
-
-# Contact Information in Sidebar
-st.sidebar.markdown('<p class="sidebar-text">Santa Clara, CA 95051<br>(408)784-9915<br>tagnev.vengat@gmail.com</p>', unsafe_allow_html=True)
-st.sidebar.markdown('[LinkedIn Profile](https://www.linkedin.com/in/vengateswaran-arunachalam/)', unsafe_allow_html=True)
-st.sidebar.markdown('[GitHub](https://github.com/tagnev)', unsafe_allow_html=True)
-st.sidebar.markdown('[Medium](https://medium.com/@tagnev.vengat)', unsafe_allow_html=True)
+# Sidebar with contact info
+st.sidebar.header('Contact Information')
+st.sidebar.write("""
+Santa Clara, CA 95051  
+[tagnev.vengat@gmail.com](mailto:tagnev.vengat@gmail.com)  
+[LinkedIn](https://www.linkedin.com/in/vengateswaran-arunachalam/)  
+[GitHub](https://github.com/tagnev)  
+[Medium](https://medium.com/@tagnev.vengat)
+""")
+st.sidebar.markdown('---')
 
 # Professional Summary
-st.markdown('<p class="subheader">Professional Summary</p>', unsafe_allow_html=True)
-st.markdown("""
-<p class="detail-text">Accomplished Full Stack Data Engineer & Data Architect with 14+ years of expertise in BI solutions. Proficient in Spark, Hadoop, Python, ReactJS, and the complete SDLC. Solid experience in cloud services with AWS & GCP, database solutions both in the cloud and on-premise, and mastery of BI tools including Tableau and MicroStrategy. Demonstrable skills in customer-focused open-source projects, content creation, and community engagement. Excel at advanced database solutions, ETL processes, and enhancing data infrastructure. Well-versed in partnering with diverse teams to foster data-driven strategies.</p>
-""", unsafe_allow_html=True)
+st.subheader('Professional Summary')
+st.write("""
+Accomplished Full Stack Data Engineer & Data Architect with 14+ years of expertise in BI solutions. 
+Proficient in Spark, Hadoop, Python, ReactJS, and the complete SDLC. Solid experience in cloud services 
+with AWS & GCP, database solutions both in the cloud and on-premise, and mastery of BI tools including 
+Tableau and MicroStrategy. Demonstrable skills in customer-focused open-source projects, content creation, 
+and community engagement. Excel at advanced database solutions, ETL processes, and enhancing data 
+infrastructure. Well-versed in partnering with diverse teams to foster data-driven strategies.
+""")
 
 # Skills
-st.markdown('<p class="subheader">Skills</p>', unsafe_allow_html=True)
-st.markdown("""
-<p class="detail-text">
-- Docker, DBT, Kubernetes, Git<br>
-- AWS & GCP architecture<br>
-- Python (Django, Flask, FastAPI)<br>
-- Shell scripting<br>
-- MySQL/Relational DB, Snowflake, Postgresql<br>
-- Hadoop, Tableau, MicroStrategy, Looker<br>
+st.subheader('Skills')
+st.write("""
+- Docker, DBT, Kubernetes, Git
+- AWS & GCP architecture
+- Python (Django, Flask, FastAPI)
+- Shell scripting
+- MySQL/Relational DB, Snowflake, Postgresql
+- Hadoop, Tableau, MicroStrategy, Looker
 - Cassandra, NoSQL, Pyspark, Airflow, Streamlit, openAI, Incorta
-</p>
-""", unsafe_allow_html=True)
+""")
 
 # Work History
-st.markdown('<p class="subheader">Work History</p>', unsafe_allow_html=True)
-# ... include your work history here, using detail-text class for paragraphs ...
+st.subheader('Work History')
+
+st.markdown('**Data Architect/Senior Data Engineer**')
+st.write('Keysight Technologies – Santa Clara, CA (10/2021 - Current)')
+st.markdown("""
+- Managed the data migration process during a merger, which included transitioning data from legacy 
+systems of both companies into a single data warehouse.
+- Developed and executed migration scripts and ETL processes to ensure data integrity and consistency.
+- Collaborated with business stakeholders to prioritize data migration efforts, identifying critical data 
+elements and dependencies.
+- Orchestrated the decommissioning of legacy systems post-migration, reducing operational costs and 
+streamlining data management.
+""")
+
+st.markdown('**Full Stack Data Engineer/Startup Founder**')
+st.write('Teckiy – Santa Clara, CA (01/2020 - 12/2022)')
+st.markdown("""
+- Collaborated with a cross-functional team to design, develop, and deploy a comprehensive end-to-end 
+data product leveraging Django as the backend framework and Google Cloud Platform (GCP) for cloud 
+infrastructure.
+- Designed and implemented a robust data pipeline on GCP, incorporating services like Google Cloud 
+Storage, BigQuery, and Cloud Dataflow for data ingestion, storage, transformation, and analysis.
+""")
+
+st.markdown('**Data Engineer Lead**')
+st.write('Keysight Technologies – Santa Clara, CA (04/2018 - 10/2021)')
+st.markdown("""
+- Designed and implemented a data lake architecture on AWS S3, providing a scalable and cost-effective 
+solution for storing and processing massive volumes of structured and unstructured data.
+- Optimized data ingestion pipelines using AWS Glue to automate data cataloging, ETL, and 
+transformation processes, resulting in a 30% reduction in data processing time.
+""")
+
+st.markdown('**Senior BI Developer**')
+st.write('Cognizant (Client: Keysight Technologies) – Santa Clara, CA (06/2014 - 04/2018)')
+st.markdown("""
+- Involved in Data Warehousing and worked extensively on RDBMS (Oracle, MS SQL) and ETL tools such 
+as Informatica, Shell Scripting, and Python.
+- Facilitated knowledge transfer sessions for new resources.
+""")
 
 # Education
-st.markdown('<p class="subheader">Education</p>', unsafe_allow_html=True)
-# ... include your education here ...
+st.subheader('Education')
+st.write('Bachelor of Science: Information Technology, 05/2009')
+st.write('Mount Zion College of Engineering & Technology - Tamilnadu, India')
+st.write('Aggregate: 80%')
 
 # Open Source Projects
-st.markdown('<p class="subheader">Open Source Projects</p>', unsafe_allow_html=True)
-# ... include your projects here ...
+st.subheader('Open Source Projects')
+st.write('[Teckiy, Developer support platform](https://www.teckiy.com)')
+st.write("[HelpU, India's first customer experience sharing portal](https://www.helpu.in)")
 
-# You can continue adding more sections using the same pattern.
-
-# Run this with `streamlit run styled_resume_app.py` in your command line
+# Run this with `streamlit run resume_app.py` in your command line
